245d0577ad97ffb75109d298f9c814cdbf61a4c7
matusbalazi <balazi@stud.uniza.sk>
2021-10-18 13:55:33 +0200

    Responzivita 1.1

--- js/headerscroll.js
+++ js/headerscroll.js
@@ -1,11 +1,29 @@
-const header = document.querySelector('.main-header');
+const hamburger = document.getElementById("hamburger-btn");
+const mainHeader = document.getElementById("id-main-header");
+hamburger.addEventListener("click", openCloseMenu);
+
+
+function openCloseMenu() {
+    if (!mainHeader.classList.contains("header-blue")) {
+        mainHeader.classList.toggle("header-blue");
+    }
+    mainHeader.classList.toggle("expand");
+}
+
+const isBlueVariant = mainHeader.classList.contains("header-blue");
 window.addEventListener('scroll', function() 
 {
+    if (!isBlueVariant) {
+        mainHeader.classList.remove("header-blue");
+    }
+
+    mainHeader.classList.remove("expand");
+
     if (window.scrollY > 300) 
     {
-        header.style.backgroundColor = '#033D67';
+        mainHeader.style.backgroundColor = '#033D67';
     }
     else {
-        header.style.backgroundColor = 'transparent';
+        mainHeader.style.backgroundColor = null;
     }
 });
